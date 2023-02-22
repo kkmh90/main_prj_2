b6e8de7faab1c8a9d2992ffc3b8fd41d5e071d8d
kkmh90 <kkmh90@naver.com>
2023-02-22 13:00:19 +0900

    타이핑효과테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import TypingText from "./components/Typing";
 
 export default function App() {
   return (
     <main>
       <Hero />
+      <TypingText />
     </main>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,30 @@
 import video from "../video/sample.mp4";
+import React from "react";
+import TypingText from "../components/Typing.jsx";
+import colors, { white } from "tailwindcss/colors";
+import "./test.css";
 
 export default function Hero() {
   return (
     <section>
-      <div>
-        <video
-          muted
-          autoPlay
-          loop
-          playsInline
-          className="z-0 h-screen w-screen"
-        >
+      <div className="relative flex items-center justify-center w-screen h-screen overflow-hidden">
+        <video muted autoPlay loop playsInline>
           <source src={video}></source>
         </video>
-      </div>
-      <div className="text-white z-10 text-center relative">
-        <p>Hero</p>
+        <div
+          className=""
+          style={{
+            position: "absolute",
+          }}
+        >
+          <TypingText
+            className="text"
+            text="오모로봇입니다."
+            speed={140}
+            fontSize="4rem"
+            color={white}
+          />
+        </div>
       </div>
     </section>
   );
